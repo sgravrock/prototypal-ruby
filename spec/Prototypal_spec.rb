fac1e6a476c632bce5fa049ae4c51e2692f869d8
Steve Gravrock <sdg@panix.com>
2016-02-03 14:57:19 -0800

    Rearranged specs

--- spec/Prototypal_spec.rb
+++ spec/Prototypal_spec.rb
@@ -1,58 +1,75 @@
 require './Prototypal'
 
 describe Prototypal do
-	context "The root object" do
-		before do
-			@subject = Prototypal.new(nil)
+	describe "Getting property values" do
+		context "From the root object" do
+			before do
+				@subject = Prototypal.new(nil)
+			end
+	
+			it "should not have a prototype" do
+				expect(@subject.proto).to be_nil
+			end
+	
+			it "should return Undefined when an unset property is read" do
+				expect(@subject.foo).to be(Undefined.value)
+			end
+	
+			it "should get its own properties" do
+				@subject.foo = 42;
+				expect(@subject.foo).to eql(42)
+			end
 		end
 
-		it "should not have a prototype" do
-			expect(@subject.proto).to be_nil
-		end
-
-		it "should return Undefined when an unset property is read" do
-			expect(@subject.foo).to be(Undefined.value)
-		end
-
-		it "should set and get its own properties" do
-			@subject.foo = 42;
-			expect(@subject.foo).to eql(42)
+		context "From an object with a prototype" do
+			before do
+				@root = Prototypal.new(nil)
+				@subject = @root.create_object
+			end
+	
+			it "should have a prototype" do
+				expect(@subject.proto).to be(@root)
+			end
+	
+			it "should get its own properties" do
+				@subject.foo = 42;
+				expect(@subject.foo).to eql(42)
+			end
+	
+			it "should return Undefined when an unset property is read" do
+				expect(@subject.foo).to be(Undefined.value)
+			end
+	
+			it "should get missing properties from the prototype" do
+				@root.foo = 17;
+				expect(@subject.foo).to eql(17)
+			end
+	
+			it "should prefer own properties over the prototype" do
+				@root.foo = 17;
+				@subject.foo = 42;
+				expect(@subject.foo).to eql(42)
+			end
 		end
 	end
 
-	context "Objects with prototypes" do
+	describe "Setting property values" do
 		before do
-			@root = Prototypal.new(nil)
-			@subject = @root.create_object
+			@subject = Prototypal.new(nil).create_object
 		end
-
-		it "should have a prototype" do
-			expect(@subject.proto).to be(@root)
-		end
-
-		it "should set and get its own properties" do
-			@subject.foo = 42;
-			expect(@subject.foo).to eql(42)
-		end
-
-		it "should return Undefined when an unset property is read" do
-			expect(@subject.foo).to be(Undefined.value)
-		end
-
-		it "should get missing properties from the prototype" do
-			@root.foo = 17;
-			expect(@subject.foo).to eql(17)
-		end
-
-		it "should prefer own properties over the prototype" do
-			@root.foo = 17;
-			@subject.foo = 42;
-			expect(@subject.foo).to eql(42)
-		end
-
-		it "should set missing properties on self, not the prototype" do
+	
+		it "should set properties on self, not the prototype" do
 			@subject.foo = 17
-			expect(@root.foo).to be(Undefined.value)
+			expect(@subject.proto.foo).to be(Undefined.value)
+			@subject.proto.foo = 42;
+			@subject.foo = 18
+			expect(@subject.proto.foo).to eql(42)
+		end
+	end
+
+	describe "respond_to?" do
+		before do
+			@subject = Prototypal.new(nil).create_object
 		end
 
 		it "responds to messages to get its own properties" do
@@ -61,7 +78,7 @@ describe Prototypal do
 		end
 
 		it "responds to messages to get its prototype's properties" do
-			@root.foo = 17
+			@subject.proto.foo = 17
 			expect(@subject.respond_to?(:foo)).to eql(true)
 		end
 
